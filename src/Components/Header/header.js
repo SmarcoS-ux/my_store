d46b0bf632dd8e4ef391392de23ffd4ef1b01943
Marcos André <88692791+SmarcoS-ux@users.noreply.github.com>
2023-10-13 23:59:02 -0300

    v0.0.2
    Add Main no projeto

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -5,12 +5,17 @@ const Header = () => {
     return(
         <header className="header">
             <div className="area-pesquisa">
-                <img id="logo" src="/assets/lista.png" alt="Logo" title="Logo-Marca"/>
-                <input type='text' placeholder='Pesquisar'/>
-                <button><img id="img-button" src="/assets/lupa.png" alt="Pesquisar" title="Pesquisar"/></button>
+                <div className='icons_1'>
+                    <img id="logo" src="/assets/lista.png" alt="Logo" title="Logo-Marca"/>
+                    <input type='text' placeholder='Pesquisar'/>
+                    <button><img id="img-button" src="/assets/lupa.png" alt="Pesquisar" title="Pesquisar"/></button>
+                </div>
             </div>
             <div className='area-user-config'>
-                <a href="#"><img id="img-user" src="/assets/user.png" alt="Usuário" title="Usuário"/></a>
+                <div className='icons'>
+                    <a href="#"><img id="config" src="/assets/engrenagem.png" alt="Config" title="Configurações"/></a>
+                    <a href="#"><img id="img-user" src="/assets/user.png" alt="Usuário" title="Usuário"/></a>
+                </div>
             </div>
         </header>
     );
--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,13 +1,86 @@
 import React from 'react';
+import "./home-style.css";
 
 import Header from '../Header/header';
 import Footer from '../Footer/footer';
 
 const Home = () => {
     return(
-        <main>
+        <main classNam="main">
             <Header/>
-            <p>Main</p>
+            <div className='banner_principal'>
+                <h1>Greeng Store</h1>
+                <h3 id="subtitle">A sua loja de Tecnologia</h3>
+            </div>
+            <div className='introducao'>
+                <div id="title-marcas">
+                    <h3>Principais marcas</h3>
+                    <div id="line"></div>
+                    <div className='marcas'>
+                        <a href="#">
+                            <div className='marca'>
+                                <img src="/assets/img-marcas/nvidia.png" alt="Nvidia" title="Nvidia"/>
+                                <p>Nvidia</p>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div className='marca'>
+                                <img src="/assets/img-marcas/amd.png" alt="Amd" title="Amd"/>
+                                <p>AMD</p>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div className='marca'>
+                                <img src="/assets/img-marcas/apple.jpg" alt="Apple" title="Apple"/>
+                                <p>Apple</p>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div className='marca'>
+                                <img src="/assets/img-marcas/asus.jpg" alt="Asus" title="Asus"/>
+                                <p>Asus</p>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div className='marca'>
+                                <img src="/assets/img-marcas/c3tech.jpg" alt="C3Tech" title="C3Tech"/>
+                                <p>C3Tech</p>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div className='marca'>
+                                <img src="/assets/img-marcas/lg.jpg" alt="LG" title="LG"/>
+                                <p>LG</p>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div className='marca'>
+                                <img src="/assets/img-marcas/microsoft.jpeg" alt="Microsoft" title="Microsoft"/>
+                                <p>Microsoft</p>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div className='marca'>
+                                <img src="/assets/img-marcas/motorola.jpg" alt="Motorola" title="Motorola"/>
+                                <p>Motorola</p>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div className='marca'>
+                                <img src="/assets/img-marcas/positivo.jpg" alt="Positivo" title="Positivo"/>
+                                <p>Positivo</p>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div className='marca'>
+                                <img src="/assets/img-marcas/asus.jpg" alt="Asus" title="Asus"/>
+                                <p>Asus</p>
+                            </div>
+                        </a>    
+                    </div>
+                </div>
+                
+            </div>
             <Footer/>
         </main>
     );
