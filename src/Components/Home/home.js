47b24c3084652521511188db770cdc821d22bd05
Marcos André <88692791+SmarcoS-ux@users.noreply.github.com>
2023-10-15 00:40:15 -0300

    v0.0.3
    Increment area main

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -7,7 +7,7 @@ import Footer from '../Footer/footer';
 const Home = () => {
     return(
         <main classNam="main">
-            <Header/>
+            <Header/> 
             <div className='banner_principal'>
                 <h1>Greeng Store</h1>
                 <h3 id="subtitle">A sua loja de Tecnologia</h3>
@@ -17,66 +17,101 @@ const Home = () => {
                     <h3>Principais marcas</h3>
                     <div id="line"></div>
                     <div className='marcas'>
-                        <a href="#">
-                            <div className='marca'>
+                        <div className='marca'>
+                            <a href="#">
                                 <img src="/assets/img-marcas/nvidia.png" alt="Nvidia" title="Nvidia"/>
                                 <p>Nvidia</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div className='marca'>
+                            </a>
+                        </div>
+                        <div className='marca'>
+                            <a href="#">
                                 <img src="/assets/img-marcas/amd.png" alt="Amd" title="Amd"/>
                                 <p>AMD</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div className='marca'>
+                            </a>
+                        </div>
+                        <div className='marca'>
+                            <a href="#">
                                 <img src="/assets/img-marcas/apple.jpg" alt="Apple" title="Apple"/>
                                 <p>Apple</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div className='marca'>
+                            </a>
+                        </div>
+                        <div className='marca'>
+                            <a href="#">
                                 <img src="/assets/img-marcas/asus.jpg" alt="Asus" title="Asus"/>
                                 <p>Asus</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div className='marca'>
+                            </a>
+                        </div>
+                        <div className='marca'>
+                            <a href="#">
                                 <img src="/assets/img-marcas/c3tech.jpg" alt="C3Tech" title="C3Tech"/>
                                 <p>C3Tech</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div className='marca'>
+                            </a>
+                        </div>
+                        <div className='marca'>
+                            <a href="#">
                                 <img src="/assets/img-marcas/lg.jpg" alt="LG" title="LG"/>
                                 <p>LG</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div className='marca'>
+                            </a>
+                        </div>
+                        <div className='marca'>
+                            <a href="#">
                                 <img src="/assets/img-marcas/microsoft.jpeg" alt="Microsoft" title="Microsoft"/>
                                 <p>Microsoft</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div className='marca'>
+                            </a>
+                        </div>
+                        <div className='marca'>
+                            <a href="#">
                                 <img src="/assets/img-marcas/motorola.jpg" alt="Motorola" title="Motorola"/>
                                 <p>Motorola</p>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div className='marca'>
+                            </a>
+                        </div>
+                        <div className='marca'>
+                            <a href="#">
                                 <img src="/assets/img-marcas/positivo.jpg" alt="Positivo" title="Positivo"/>
                                 <p>Positivo</p>
+                            </a>
+                        </div>
+                        <div className='marca'>
+                            <a href="#">
+                                <img src="/assets/img-marcas/samsung.jpg" alt="Samsung" title="Samsung"/>
+                                <p>Samsung</p>
+                            </a>  
+                        </div>
+                    </div>
+                    <div className='area-main'>
+                        <div className='filtros'>
+                            <div className='filtro'>
+                                <h4>Condições:</h4>
+                                <label><input type="checkbox"/>Usado</label>
+                                <label><input type="checkbox"/>Novo</label>
                             </div>
-                        </a>
-                        <a href="#">
-                            <div className='marca'>
-                                <img src="/assets/img-marcas/asus.jpg" alt="Asus" title="Asus"/>
-                                <p>Asus</p>
+                            <div className='filtro'>
+                                <h4>Preços:</h4>
+                                <label><input type="checkbox"/>Até R$ 25,00</label>
+                                <label><input type="checkbox"/>De R$ 25,00 à R$ 100,00</label>
+                                <label><input type="checkbox"/>De R$ 100,00 à R$ 300,00</label>
+                                <label><input type="checkbox"/>De R$ 300,00 à R$ 500,00</label>
+                                <label><input type="checkbox"/>De R$ 500,00 à R$ 800,00</label>
+                                <label><input type="checkbox"/>De R$ 800,00 à R$ 1.500,00</label>
+                                <label><input type="checkbox"/>De R$ 1.500,00 à R$ 3.000,00</label>
+                                <label><input type="checkbox"/>Acima de R$ 3.000,000</label>
                             </div>
-                        </a>    
+                            <div className='filtro'>
+                                <h4>Tipo de Produto:</h4>
+                                <label><input type="checkbox"/>Smartphones</label>
+                                <label><input type="checkbox"/>Computadores</label>
+                                <label><input type="checkbox"/>Fones de ouvido</label>
+                                <label><input type="checkbox"/>Impressoras</label>
+                                <label><input type="checkbox"/>Cabos e Adaptadores</label>
+                                <label><input type="checkbox"/>Rede e Internet</label>
+                            </div>
+                        </div>
+                        <div className='frete'>
+                            <img src="/assets/frete.png" alt="Frete" title="Frete"/>
+                            <p>Frete grátis em quase todos os produtos da loja. Aproveite!</p>
+                            <p>Nossos produtos são enviados no mesmo dia do pagamento.</p>
+                            <p>Enviamos para todo o Brasil e algumas localidades internacionais sem taxa e custos adicionais.</p>
+                        </div>
                     </div>
                 </div>
                 
